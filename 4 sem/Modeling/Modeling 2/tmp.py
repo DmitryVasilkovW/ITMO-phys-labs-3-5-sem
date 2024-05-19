6458b9fb933c8a4b390316858cdbe26af4aa4c86
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-05-19 22:51:13 +0300

    fix: modeling 2

--- 4 sem/Modeling/Modeling 2/tmp.py
+++ 4 sem/Modeling/Modeling 2/tmp.py
@@ -1,41 +1,14 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.colors import LinearSegmentedColormap, Normalize
+from mpl_toolkits.mplot3d import Axes3D
 
 period = 1.0
 total_strokes = 1000
 
 theta = np.linspace(-np.pi / 2, np.pi / 2, 100)
-wavelength = np.linspace(400, 750, 2000)
+wavelength = np.linspace(450, 495, 2000)  # Измените эту строку
 
-Theta, Wavelength = np.meshgrid(theta, wavelength)
-
-Intensity = np.sin(np.pi * total_strokes * period * np.sin(Theta) / Wavelength) ** 2 / \
-            (np.sin(np.pi * period * np.sin(Theta) / Wavelength) ** 2)
-
-colors = [(0.0, 'violet'), (0.15, 'indigo'), (0.3, 'blue'),
-          (0.45, 'green'), (0.6, 'yellow'),
-          (0.75, 'orange'), (1.0, 'red')]
-cmap_wave = LinearSegmentedColormap.from_list('wave', colors)
-
-norm = Normalize(vmin=wavelength.min(), vmax=wavelength.max())
-
-fig2 = plt.figure(figsize=(12, 6))
-
-ax1 = fig2.add_subplot(121, projection='3d')
-surf1 = ax1.plot_surface(Theta, Wavelength, Intensity, facecolors=cmap_wave(norm(Wavelength)))
-ax1.set_xlabel('Угол дифракции')
-ax1.set_ylabel('Длина волны, нм')
-ax1.set_zlabel('Интенсивность')
-
-ax2 = fig2.add_axes([0.91, 0.1, 0.02, 0.8])
-cbar = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap_wave), cax=ax2)
-cbar.set_label('Длина волны, нм')
-
-plt.tight_layout()
-plt.show()
-
-wavelength = np.linspace(450, 495, 2000)
 Theta, Wavelength = np.meshgrid(theta, wavelength)
 
 # Выберите длину волны для синего цвета (около 470 нм)
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 # Механика, Молекулярная физика, Термодинамика
 
-## [формулы](images/Скан 2.pdf) ##
+## [формулы](https://github.com/DmitryVasilkovW/ITMO-phys-labs-3-5-sem/blob/18d805a3dd59c486a1f47c4c50899a71444cfabf/images/%D0%A1%D0%BA%D0%B0%D0%BD%202.pdf) ##
 
 # Электродинамика, Постоянный ток, Магнитизм, Переменный ток, Оптика, Э/М волны, Квантовая физика
 
-## [формулы](images/Скан 1.pdf) ##
+## [формулы](https://github.com/DmitryVasilkovW/ITMO-phys-labs-3-5-sem/blob/18d805a3dd59c486a1f47c4c50899a71444cfabf/images/%D0%A1%D0%BA%D0%B0%D0%BD%201.pdf) ##
