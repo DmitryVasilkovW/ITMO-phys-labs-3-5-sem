18d805a3dd59c486a1f47c4c50899a71444cfabf
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-05-19 21:30:35 +0300

    feat: modeling 2

---
+++ 4 sem/Modeling/Modeling 2/modeling 2.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap, Normalize
+
+period = 1.0
+total_strokes = 1000
+
+theta = np.linspace(-np.pi / 2, np.pi / 2, 100)
+wavelength = np.linspace(400, 750, 2000)
+
+Theta, Wavelength = np.meshgrid(theta, wavelength)
+
+Intensity = np.sin(np.pi * total_strokes * period * np.sin(Theta) / Wavelength) ** 2 / \
+            (np.sin(np.pi * period * np.sin(Theta) / Wavelength) ** 2)
+
+colors = [(0.0, 'blue'), (0.5, 'green'), (0.8, 'yellow'), (1.0, 'red')]
+cmap_wave = LinearSegmentedColormap.from_list('wave', colors)
+
+# Нормализуем данные по длине волны
+norm = Normalize(vmin=wavelength.min(), vmax=wavelength.max())
+
+fig2 = plt.figure(figsize=(12, 6))
+
+# График с основным содержимым
+ax1 = fig2.add_subplot(121, projection='3d')
+surf1 = ax1.plot_surface(Theta, Wavelength, Intensity, cmap='plasma')
+ax1.set_xlabel('Угол дифракции')
+ax1.set_ylabel('Длина волны, нм')
+ax1.set_zlabel('Интенсивность')
+
+# Создание отдельного графика для цветовой шкалы
+ax2 = fig2.add_axes([0.91, 0.1, 0.02, 0.8])
+cbar = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap_wave), cax=ax2)
+cbar.set_label('Длина волны, нм')
+
+plt.tight_layout()
+plt.show()
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 # Механика, Молекулярная физика, Термодинамика
 
-## [формулы](https://github.com/DmitryVasilkovW/ITMO-phys-labs-3-5-sem/blob/main/3%20%D1%81%D0%B5%D0%BC/Tests%20from%20lectures/%D0%A1%D0%BA%D0%B0%D0%BD%202.pdf) ##
+## [формулы](images/Скан 2.pdf) ##
 
 # Электродинамика, Постоянный ток, Магнитизм, Переменный ток, Оптика, Э/М волны, Квантовая физика
 
-## [формулы](https://github.com/DmitryVasilkovW/ITMO-phys-labs-3-5-sem/blob/main/3%20%D1%81%D0%B5%D0%BC/Tests%20from%20lectures/%D0%A1%D0%BA%D0%B0%D0%BD%201.pdf) ##
+## [формулы](images/Скан 1.pdf) ##
